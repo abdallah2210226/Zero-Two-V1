0745d5672c784b586aabaa7e5d410278ecab0b2f
abdallah2210226 <53609641+abdallah2210226@users.noreply.github.com>
2023-06-27 17:35:17 +0200

    Create var.py

---
+++ module/var.py
@@ -0,0 +1,94 @@
+ping_photo = "https://t.me/hohcusc9us/242"
+start_photo = "https://t.me/hohcusc9us/244"
+help_photo = "https://t.me/hohcusc9us/252"
+anime_menu = "https://t.me/hohcusc9us/246"
+search = "https://t.me/hohcusc9us/248"
+profile_pic = "https://t.me/hohcusc9us/250"
+rulesphoto = "https://t.me/hohcusc9us/152"
+
+models = {
+"Analog V1": "analog-diffusion-1.0.ckpt [9ca13f02]",
+"Anything V3": "anythingv3_0-pruned.ckpt [2700c435]",
+"Anything V4.5": "anything-v4.5-pruned.ckpt [65745d25]",
+"Anything V5": "anythingV5_PrtRE.safetensors [893e49b9]",
+"AbyssOrangeMix V3": "AOM3A3_orangemixs.safetensors [9600da17]",
+"Deliberate V2": "deliberate_v2.safetensors [10ec4b29]",
+"Dreamlike Diffusion V1": "dreamlike-diffusion-1.0.safetensors [5c9fd6e0]",
+"Dreamlike Diffusion V2": "dreamlike-diffusion-2.0.safetensors [fdcf65e7]",
+"Dreamshaper 5 baked vae": "dreamshaper_5BakedVae.safetensors [a3fbf318]",
+"Dreamshaper 6 baked vae": "dreamshaper_6BakedVae.safetensors [114c8abb]",
+"Elldreth's Vivid": "elldreths-vivid-mix.safetensors [342d9d26]",
+"Lyriel V1.5": "lyriel_v15.safetensors [65d547c5]",
+"Lyriel V1.6": "lyriel_v16.safetensors [68fceea2]",
+"MeinaMix Meina V9": "meinamix_meinaV9.safetensors [2ec66ab0]",
+"Openjourney V4": "openjourney_V4.ckpt [ca2f377f]",
+"Portrait V1": "portrait+1.0.safetensors [1400e684]",
+"Realistic Vision V1.4": "Realistic_Vision_V1.4-pruned-fp16.safetensors [8d21810b]",
+"Realistic Vision V2.0": "Realistic_Vision_V2.0.safetensors [79587710]",
+"ReV Animated V1.2.2": "revAnimated_v122.safetensors [3f4fefd9]",
+"SD V1.4": "sdv1_4.ckpt [7460a6fa]",
+"SD V1.5": "v1-5-pruned-emaonly.ckpt [81761151]",
+"Shonin's Beautiful People V1.0": "shoninsBeautiful_v10.safetensors [25d8c546]",
+"TheAlly's Mix II": "theallys-mix-ii-churned.safetensors [5d9225a4]",
+"Timeless V1": "timeless-1.0.ckpt [7c4971d4]"
+}
+banned_words = ["booty","2g1c", "2 girls 1 cup", "acrotomophilia", "alabama hot pocket", "alaskan pipeline", "anal", "anilingus", "anus", "apeshit", "arsehole", "ass", "asshole", "assmunch", "auto erotic", "autoerotic", "babeland", "baby batter", "baby juice", "ball gag", "ball gravy", "ball kicking", "ball licking", "ball sack", "ball sucking", "bangbros", "bareback", "barely legal", "barenaked", "bastard", "bastardo", "bastinado", "bbw", "bdsm", "beaner", "beaners", "beaver cleaver", "beaver lips", "bestiality", "big black", "big breasts", "big knockers", "big tits", "bimbos", "birdlock", "bitch", "bitches", "black cock", "blonde action", "blonde on blonde action", "blowjob", "blow job", "blow your load", "blue waffle", "blumpkin", "bollocks", "bondage", "boner", "boob", "boobs", "booty call", "brown showers", "brunette action", "bukkake", "bulldyke", "bullet vibe", "bullshit", "bung hole", "bunghole", "busty", "butt", "buttcheeks", "butthole", "camel toe", "camgirl", "camslut", "camwhore", "carpet muncher", "carpetmuncher", "chocolate rosebuds", "circlejerk", "cleveland steamer", "clit", "clitoris", "clover clamps", "clusterfuck", "cock", "cocks", "coprolagnia", "coprophilia", "cornhole", "coon", "coons", "creampie", "cum", "cumming", "cunnilingus", "cunt", "darkie", "date rape", "daterape", "deep throat", "deepthroat", "dendrophilia", "dick", "dildo", "dingleberry", "dingleberries", "dirty pillows", "dirty sanchez", "doggie style", "doggiestyle", "doggy style", "doggystyle", "dog style", "dolcett", "domination", "dominatrix", "dommes", "donkey punch", "double dong", "double penetration", "dp action", "dry hump", "dvda", "eat my ass", "ecchi", "ejaculation", "erotic", "erotism", "escort", "eunuch", "faggot", "fecal", "felch", "fellatio", "feltch", "female squirting", "femdom", "figging", "fingerbang", "fingering", "fisting", "foot fetish", "footjob", "frotting", "fuck", "fuck buttons", "fuckin", "fucking", "fucktards", "fudge packer", "fudgepacker", "futanari", "gang bang", "gay sex", "genitals", "giant cock", "girl on", "girl on top", "girls gone wild", "goatcx", "goatse", "god damn", "gokkun", "golden shower", "goodpoop", "goo girl", "goregasm", "grope", "group sex", "g-spot", "guro", "hand job", "handjob", "hard core", "hardcore", "hentai", "homoerotic", "honkey", "hooker", "hot carl", "hot chick", "how to kill", "how to murder", "huge fat", "humping", "incest", "intercourse", "jack off", "jail bait", "jailbait", "jelly donut", "jerk off", "jigaboo", "jiggaboo", "jiggerboo", "jizz", "juggs", "kike", "kinbaku", "kinkster", "kinky", "knobbing", "leather restraint", "leather straight jacket", "lemon party", "lolita", "lovemaking", "make me come", "male squirting", "masturbate", "menage a trois", "milf", "missionary position", "motherfucker", "mound of venus", "mr hands", "muff diver", "muffdiving", "nambla", "nawashi", "negro", "neonazi", "nigga", "nigger", "nig nog", "nimphomania", "nipple", "nipples", "nsfw images", "nude", "nudity", "nympho", "nymphomania", "octopussy", "omorashi", "one cup two girls", "one guy one jar", "orgasm", "orgy", "paedophile", "paki", "panties", "panty", "pedobear", "pedophile", "pegging", "penis", "phone sex", "piece of shit", "pissing", "piss pig", "pisspig", "playboy", "pleasure chest", "pole smoker", "ponyplay", "poof", "poon", "poontang", "punany", "poop chute", "poopchute", "porn", "porno", "pornography", "prince albert piercing", "pthc", "pubes", "pussy", "queaf", "queef", "quim", "raghead", "raging boner", "rape", "raping", "rapist", "rectum", "reverse cowgirl", "rimjob", "rimming", "rosy palm", "rosy palm and her 5 sisters", "rusty trombone", "sadism", "santorum", "scat", "schlong", "scissoring", "semen", "sex", "sexo", "sexy", "shaved beaver", "shaved pussy", "shemale", "shibari", "shit", "shitblimp", "shitty", "shota", "shrimping", "skeet", "slanteye", "slut", "s&m", "smut", "snatch", "snowballing", "sodomize", "sodomy", "spic", "splooge", "splooge moose", "spooge", "spread legs", "spunk", "strap on", "strapon", "strappado", "strip club", "style doggy", "suck", "sucks", "suicide girls", "sultry women", "swastika", "swinger", "tainted love", "taste my", "tea bagging", "threesome", "throating", "tied up", "tight white", "tit", "tits", "titties", "titty", "tongue in a", "topless", "tosser", "towelhead", "tranny", "tribadism", "tub girl", "tubgirl", "tushy", "twat", "twink", "twinkie", "two girls one cup", "undressing", "upskirt", "urethra play", "urophilia", "vagina", "venus mound", "vibrator", "violet wand", "vorarephilia", "voyeur", "vulva", "wank", "wetback", "wet dream", "white power", "wrapping men", "wrinkled starfish", "xx", "xxx", "yaoi", "yellow showers", "yiffy", "zoophilia", "🖕","كسمك", "سكس", "طيز", "شرج", "لعق", "لحس", "مص", "تمص", "بيضان", "ثدي", "بز", "بزاز", "حلمة", "مفلقسة", "بظر", "كس", "فرج", "شهوة", "شاذ", "مبادل", "عاهرة", "جماع", "قضيب", "زب", "لوطي", "لواط", "سحاق", "سحاقية", "اغتصاب", "خنثي", "احتلام", "نيك", "متناك", "متناكة", "شرموطة", "عرص", "خول", "قحبة", "لبوة", "تتناك", "قحبه", "زبر","عرث"]
+
+ping="""
+⿻ بوت ❲ [{}](https://t.me/{}) ❳  يتم الان تشغيلة علي بينج ↤ ❲ {} ms ❳  .
+⿻ البوت قيد التشغيل من ❲ {} ❳ .
+⿻ يتم استخدام ❲ {} ❳ من مساحة السيرفر . 
+⿻ يتم استهلاك ❲ {} ❳ من المعالج , ويتم استهلاك ❲ {} ❳ من الرام .
+"""
+unsupscribe =  "أنت لم تشترك في القنوات المطلوبة. الرجاء الاشتراك في جميع القنوات المطلوبة لاستخدام هذا البوت."
+start_message = """
+⿻ أوهايو {} 👋🏻🎌.
+⿻ أهلا بك في بوت ↤ ❲ [{}](https://t.me/{}) ❳  .
+⿻ أرسل اسم الانمي لعرض النتائج .
+⿻ اضغط علي زر ( بحث عن الانمي )  لعرض نتائج الانمي حسب ( التصنيف , الحرف , النوع ) .
+⿻ يمكنك استخدام امر /ai + (الوصف) لتوليد صور بالذكاء الاصطناعي . 
+⿻ مطور البوت  ↤ ❲ [{}](https://t.me/{}) ❳  .
+⿻ البوت قيد التشغيل من ❲ {} ❳ .
+"""
+
+rules = """
+- اهلا بك في جروب  <a href="t.me/{}">{}</a>
+القوانين :
+- أحترام آدمن والمالك
+- يمنع السب تنحظر🔇
+- منع نشر الروابط📥
+- منع نشر المحتوى اباحي ودموي🔞
+- منع ألتحدث بالسياسة والدين❌
+- تخرب المجموعة نخرب وجهك
+💙بقروب كلنا أصدقاء وتشرفونا❤️
+"""
+id_msg = """ ╭━━❰𝐔𝐒𝐄𝐑 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍❱━━➣
+┣⪼  𝐍𝐀𝐌𝐄 ⌯ <a href="tg://user?id={}">{}</a>
+┣⪼  𝐔𝐒𝐄𝐑 ⌯ {}
+┣⪼  𝐈𝐃 ⌯ <code>{}</code>
+┣⪼  𝐓𝐘𝐏𝐄 ⌯ {}
+┣⪼  𝐃𝐀𝐓𝐄 𝐌𝐒𝐆 ⌯ {}
+┣⪼  𝐁𝐈𝐎 ⌯ {} """
+profile_message = """*مرحبًا بك في الملف الشخصي!
+يرجى اختيار الخيار المناسب :*"""
+Quote= """
+═══════ ≪ °❈° ≫ ═══════
+⛩┇› A Random Quote Was Found For You
+⛩┇› Anime : {} .
+⛩┇› Say : {} .
+⛩┇› Quote : \n`{}`
+        ══════════════
+⛩┇› أخترت لك إقتباس 🗣
+⛩┇› من أنمي : {} .
+⛩┇› القائل : {} .
+⛩┇› الأقتباس :\n `{}` .
+═══════ ≪ °❈° ≫ ═══════
+"""
+anime_msg = """
+⿻ الاسم : ❲{}❳
+⿻ القصة : 
+    ◎ ─━───━─ 𖡦─━───━─ ◎ 
+    {}
+    ◎ ─━───━─ 𖡦─━───━─ ◎ 
+⿻ التصنيف : {}
+{}"""
